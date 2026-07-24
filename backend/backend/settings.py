e44f5bcb4ce4e08bb963c680a0171eb84d496780
satyaveer <satyaveer.negi@gmail.com>
2026-07-24 20:10:12 +0530

    Fix preflight pre-check validation failure by explicitly mapping allowed CORS origins and enabling credentials control

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 DATABASES = {
     'default': {
